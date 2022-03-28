3ed9fc570380e32ace66ab909338e7ff5d639519
LUNA-AS <luabousaad@gmail.com>
2022-03-28 19:23:10 +0100

    Added new observation activity and initial Observation class. Added Glide library to gradle.

---
+++ project/app/src/main/java/com/example/bonbon/NewObservation.java
@@ -0,0 +1,15 @@
+package com.example.bonbon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewObservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_observation);
+
+    }
+}
---
+++ project/app/src/main/java/com/example/bonbon/data_models/Observation.java
@@ -0,0 +1,10 @@
+package com.example.bonbon.data_models;
+
+import java.util.ArrayList;
+
+public class Observation {
+    ArrayList<String> tags;
+    String body;
+    long timeStamp;
+    String image;
+}
