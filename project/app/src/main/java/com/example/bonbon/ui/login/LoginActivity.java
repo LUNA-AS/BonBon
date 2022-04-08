7b28794174beec3fe53e314cdc7013309e7c540b
LUNA-AS <luabousaad@gmail.com>
2022-04-08 14:47:08 +0100

    Added biometrics to login

--- project/app/src/main/java/com/example/bonbon/ui/login/LoginActivity.java
+++ project/app/src/main/java/com/example/bonbon/ui/login/LoginActivity.java
@@ -1,18 +1,28 @@
 package com.example.bonbon.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.bonbon.MainActivity;
 import com.example.bonbon.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.concurrent.Executor;
+
 public class LoginActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    Executor executor;
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,35 @@ public class LoginActivity extends AppCompatActivity {
         // Check if user is logged in
         FirebaseUser firebaseUser = mAuth.getCurrentUser();
         if (firebaseUser != null) {
-            //startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+            executor = ContextCompat.getMainExecutor(LoginActivity.this);
+            biometricPrompt = new androidx.biometric.BiometricPrompt(LoginActivity.this, executor, new androidx.biometric.BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    Toast.makeText(LoginActivity.this, "Authentication Error: " + errString, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull androidx.biometric.BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    // TODO log activity
+                }
+            });
+            promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric Authentication")
+                    .setSubtitle("Scan your fingerprint to proceed")
+                    .setNegativeButtonText("Return")
+                    .build();
+            biometricPrompt.authenticate(promptInfo);
+
         }
     }
 }
--- project/app/src/main/java/com/example/bonbon/ui/login/LoginFragment.java
+++ project/app/src/main/java/com/example/bonbon/ui/login/LoginFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,6 +19,8 @@ import android.widget.Toast;
 import com.example.bonbon.MainActivity;
 import com.example.bonbon.R;
 import com.example.bonbon.data_management.Encryption;
+import com.example.bonbon.ui.profiles.PupilProfileActivity;
+import com.example.bonbon.ui.profiles.ViewContacts;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -25,8 +29,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.concurrent.Executor;
+
 public class LoginFragment extends Fragment {
     FirebaseAuth mAuth;
+    Executor executor;
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+
 
     public LoginFragment() {
         // Required empty public constructor
@@ -114,6 +124,8 @@ public class LoginFragment extends Fragment {
                         .commit();
             }
         });
+
+
         return view;
     }
 }
