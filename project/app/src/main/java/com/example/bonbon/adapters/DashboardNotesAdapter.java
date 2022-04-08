b5f298b60468ec5ecf9a5160752e52b0a25a0d9d
LUNA-AS <luabousaad@gmail.com>
2022-04-08 10:59:37 +0100

    Added adapters for the dashboard recycler view and pager

---
+++ project/app/src/main/java/com/example/bonbon/adapters/DashboardNotesAdapter.java
@@ -0,0 +1,76 @@
+package com.example.bonbon.adapters;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bonbon.R;
+import com.example.bonbon.ViewObservationActivity;
+import com.example.bonbon.data_models.Observation;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+public class DashboardNotesAdapter extends RecyclerView.Adapter<DashboardNotesAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<Observation> observations;
+    private SimpleDateFormat sdf;
+
+    public DashboardNotesAdapter(Context context, ArrayList<Observation> observations) {
+        this.context = context;
+        this.observations = observations;
+        sdf = new SimpleDateFormat("d/MMM/yy hh:mm");
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.observation_dashboard_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        String formatted = sdf.format(observations.get(position).getTimeStamp());
+        holder.time.setText(formatted);
+        holder.body.setText(observations.get(position).getBody());
+        holder.container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ViewObservationActivity.class);
+                intent.putExtra("body", observations.get(position).getBody());
+                intent.putExtra("tags", observations.get(position).getTags());
+                intent.putExtra("image", observations.get(position).getImage());
+                intent.putExtra("timestamp", observations.get(position).getTimeStamp());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return observations.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView time, body;
+        ConstraintLayout container;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            time = itemView.findViewById(R.id.observationDateDashboard);
+            body = itemView.findViewById(R.id.observationBodyDashboard);
+            container = itemView.findViewById(R.id.dashboardNoteContainer);
+        }
+    }
+}
---
+++ project/app/src/main/java/com/example/bonbon/adapters/DashboardPagerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.bonbon.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.bonbon.R;
+import com.example.bonbon.data_models.Child;
+
+import java.util.ArrayList;
+
+public class DashboardPagerAdapter extends PagerAdapter {
+    Context context;
+    ArrayList<Child> children;
+
+    public DashboardPagerAdapter(Context context, ArrayList<Child> children) {
+        this.context = context;
+        this.children = children;
+    }
+
+    public void setChildren(ArrayList<Child> children) {
+        this.children = children;
+    }
+
+    @Override
+    public int getCount() {
+        return children.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.require_attention_item, container, false);
+        TextView name = view.findViewById(R.id.pagerPupilName);
+        TextView description = view.findViewById(R.id.pagerDescription);
+        name.setText(children.get(position).getFirstName()+" "+children.get(position).getLastName());
+        description.setText("Score: " + children.get(position).getAvgScore());
+        try {
+            container.addView(view, position);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
+}
